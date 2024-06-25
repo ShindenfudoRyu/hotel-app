98683b8309e515c4eaba53cfbb8f52252291171e
Azhr <azhr95@hotmail.com>
2024-06-24 21:25:26 -0400

    styling the app

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Reservation } from '../models/reservation';
+import { ReservationService } from '../reservation/reservation.service';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrls: ['./reservation-list.component.css']
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit{
+
+  //private reservations : Reservation [] = [];
+  reservations : Reservation [] = [];
+  //private reservationService : ReservationService = new ReservationService();
+  
+  // constructor(){
+  //   let savedReservations = localStorage.getItem("reservations")
+  //   this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  // }
+
+  constructor(private reservationService:ReservationService){
+
+  }
+
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getReservations();
+  }
+
+  // getReservations() : Reservation[] {
+  //   return this.reservations;
+  // }
+
+  deleteReservation(id:string): void{
+    this.reservationService.deleteReservation(id);
+  }
 
 }
